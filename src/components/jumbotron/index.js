f110d59cfb231e3515ee2fce0ddb2b3bc582c574
Teinovic <momir.teinovic.2011@gmail.com>
2021-04-03 17:39:44 +0200

    making my compound component show up on my browser page

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Inner, Item, Container } from './styles/jumbotron'
+import { Inner, Item, Container, Title, SubTitle, Image } from './styles/jumbotron'
 
 
 export default function Jumbotron({ children, direction = 'row', ...restProps }) {
@@ -16,4 +16,16 @@ export default function Jumbotron({ children, direction = 'row', ...restProps })
 
 Jumbotron.Container = function JumbotronContainer ({children, ...restProps}) {
     return <Container {...restProps}>{children}</Container>
+}
+
+Jumbotron.Title = function JumbotronTitle ({children, ...restProps}) {
+    return <Title {...restProps}>{children}</Title>
+}
+ 
+Jumbotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+    return <SubTitle {...restProps}>{children}</SubTitle>;
+}
+
+Jumbotron.Image = function JumbotronImage({ ...restProps }) {
+    return <Image {...restProps } />;
 }
