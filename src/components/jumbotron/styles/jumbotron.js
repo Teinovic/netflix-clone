1f0ec5d9004fea7ca8fd78e8877e8701bd7e8a1b
Teinovic <momir.teinovic.2011@gmail.com>
2021-04-05 23:12:11 +0200

    creating the footer component

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -46,6 +46,7 @@ export const Title = styled.h1`
     font-size: 50px;
     line-height: 1.1;
     margin-bottom: 8px;
+    
 
     @media (max-width: 600px) {
         font-size: 35px;
@@ -56,6 +57,7 @@ export const SubTitle = styled.h2`
     font-size: 26px;
     font-weight: normal;
     line-height: normal;
+    
 
     @media (max-width: 600px) {
         font-size: 18px;
@@ -65,4 +67,5 @@ export const SubTitle = styled.h2`
 export const Image = styled.img`
     max-width: 100%;
     height: auto;
+    
  `
